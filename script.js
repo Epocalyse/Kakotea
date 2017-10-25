cc04ab1e5718df169d1ea938a12755f37a10071a
Thispered <hcohen123@gmail.com>
2017-10-25 21:15:00 +0100

    Todo for js

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function verifyemail() {
             document.getElementById('error').innerHTML = "Please enter an email not in use.";
         }
     }
+
+//take email and 'login'
+//take signup and make key
+//email new user
+//logged in mode, aka pass cookie
+
+//display products on homepage
+//add order to basket, must be logged in to do so
+//update sidebar basket
+
+//display user prev. orders
+//display user basket (live orders)
+//submit orders into db
+//take 
