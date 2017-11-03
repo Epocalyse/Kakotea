6f50447f2a39a5053d2e6c8fcb8f78f7ba1b78e8
AverageMemer <33128238+AverageMemer@users.noreply.github.com>
2017-11-03 23:56:22 +0000

    Add files via upload

--- KakoTea.sql
+++ KakoTea.sql
@@ -1,33 +1,43 @@
-DROP TABLE Orders;
-DROP TABLE Products;
-DROP TABLE Users;
+-- Alex Drogemuller and Hannah Cohen
 
-CREATE TABLE Users (
-	Email varchar(255) NOT NULL PRIMARY KEY,
-	Password varchar(255) NOT NULL,
-	Address varchar(255) NOT NULL,
-	FirstName varchar(255) NOT NULL,
-	LastName varchar(255) NOT NULL
-);
-
-CREATE TABLE Products (
+CREATE TABLE IF NOT EXISTS Products (
 	ProductID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	ProductName varchar(255) NOT NULL,
-	ProductDes varchar(255) NOT NULL,
 	ProductPrice int NOT NULL
 );
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
 	OrderNum int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	ProdID int NOT NULL,
-	E_mail varchar(255),
-	LiveOrder bit,
-	FOREIGN KEY (ProdID) REFERENCES Products(ProductID),
-	FOREIGN KEY (E_mail) REFERENCES Users(Email)
+	Quantity int NOT NULL,
+	PhoneNum varchar(255) NOT NULL,
+	Email varchar(255) NOT NULL,
+	Address varchar(255) NOT NULL,
+	FirstName varchar(255) NOT NULL,
+	LastName varchar(255) NOT NULL,
+	FOREIGN KEY (ProdID) REFERENCES Products(ProductID)
 );
 
-INSERT INTO Products (ProductName, ProductDes, ProductPrice)
-VALUES ('Red Porcelain Tea Set', '', 55.00);
+INSERT INTO Products (ProductName, ProductPrice)
+VALUES ("Spring Flowers Tea Set", 35.45);
 
-INSERT INTO Products (ProductName, ProductDes, ProductPrice)
-VALUES ('Blue Ceramic Tea and Sushi Set', '', 80.00);
+INSERT INTO Products (ProductName, ProductPrice)
+VALUES ("Ebony Twin Tea Set", 19.99);
+
+INSERT INTO Products (ProductName, ProductPrice)
+VALUES ("Ceramic Sushi and Tea Party Set", 120.00);
+
+INSERT INTO Products (ProductName, ProductPrice)
+VALUES ("Blue Ceramic Tea and Sushi Set", 27.99);
+
+INSERT INTO Products (ProductName, ProductPrice)
+VALUES ("Traditional Clay Tea Set", 15.00);
+
+INSERT INTO Products (ProductName, ProductPrice)
+VALUES ("Red Porcelain Tea Set", 35.99);
+
+INSERT INTO Orders (ProdID, Quantity, PhoneNum, Email, Address, FirstName, LastName)
+VALUES (1, 2, 12345678910, "hi@gmail.com", "1 House", "Alex", "Meme");
+
+INSERT INTO Orders (ProdID, Quantity, PhoneNum, Email, Address, FirstName, LastName)
+VALUES (4, 3, 12345678910, "hi@gmail.com", "1 House", "Alex", "Meme");
--- sqlquery.js
+++ sqlquery.js
@@ -1,7 +1,15 @@
+//Alex Drogemuller and Hannah Cohen
 var mysql = require('mysql');
 var express = require('express');
 var http = require('http');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({
+	extented: true
+}));
+
+app.use(bodyParser.json());
 
 var connection = mysql.createConnection({
 	host: 'mysql-server-1',
@@ -11,11 +19,12 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-app.get('/searchOrder', function (req, res) {
-  var s = req.query.search.trim();
-  if ( s.indexOf(";") != -1 ) s = "*"; // stop injection attack
-  if ( s != "*" ) { var q = "select * from Orders where E_mail='"
-   + s + "' and LiveOrder=0";
+app.post('/searchOrder', function (req, res) {
+  var s = req.body.Email;
+  console.log(s);
+  if ( s.indexOf(";") == -1 ) {
+  var q = "SELECT * FROM Orders WHERE Email='"
+   + s + "'";
   connection.query(q, function (err, results, fields) {
     if (err) { res.send('error querying: ' + err); return; }
     var s = "<!DOCTYPE html>\n<html>\n<head>\n<title>Orders</title>\n</head>\n<body>\n<table border='1'>\n<tr>";
@@ -33,9 +42,9 @@ app.get('/searchOrder', function (req, res) {
   }
 });
 
-app.get('/checkout', function (req, res) {
-	var email = req.query.email.trim();
-	var q = "select ProdID from Orders where E_mail='" + email + "' and LiveOrder=1";
+app.post('/checkout', function (req, res) {
+	var email = req.query.Email;
+	var q = "select ProdID from Orders where Email='" + email + "' and LiveOrder=1";
 	connection.query(q, function (err, results, fields) {
 		if (err) { res.send('error querying: ' + err); return; }
     var s = "<!DOCTYPE html>\n<html>\n<head>\n<title>Checkout</title>\n</head>\n<body>\n<table border='1'>\n<tr>";
@@ -51,70 +60,28 @@ app.get('/checkout', function (req, res) {
     res.send(s);
 	});
 });
-
-app.get('/addOrder', function (req, res) {
-   var prodID = req.query.prodID.trim();
-   var email = req.query.email.trim();
-   var q = "insert into Orders (ProdID, E_mail, LiveOrder) values ";
-   if ( prodID.indexOf(";") == -1 && email.indexOf(";") == -1 )
-        q += "('" + prodID + "', '" + email + "', 1 )";
-   else q = "";
-   connection.query(q, function (err, results, fields) {
-     if (err) { res.end('error adding order: ' + err); return; }
-     res.end("<!DOCTYPE html>\n<html>\n<head>\n<title>Orders</title>\n</head>\n<body>\n<p>" + results.affectedRows +
-             " record added</p>\n</body>\n</html>\n");
-   });
- });
  
- app.get('/addUser', function (req, res) {
-   var email = req.query.email.trim();
-   var password = req.query.password.trim();
-   var address = req.query.address.trim();
-   var firstName = req.query.firstName.trim();
-   var lastName = req.query.lastName.trim();
-   var q = "insert into Users (Email, Password, Address, FirstName, LastName) values ";
-   if ( email.indexOf(";") == -1 && password.indexOf(";") == -1 && address.indexOf(";") == -1
+ app.post('/addOrder', function (req, res) {
+ 	var prodID = req.query.ProdID;
+ 	var phoneNum = req.query.PhoneNo;
+   var email = req.query.Email;
+   var address = req.query.Address;
+   var firstName = req.query.FirstName;
+   var lastName = req.query.LastName;
+   
+   var q = "insert into Orders (ProdID, PhoneNum, Email, Address, FirstName, LastName) values ";
+   if ( email.indexOf(";") == -1 && phoneNum.indexOf(";") == -1 && address.indexOf(";") == -1
     && firstName.indexOf(";") == -1 && lastName.indexOf(";") == -1 )
-        q += "('" + email + "', '" + password + "', '" + address + "', '"
+        q += "('" + prodID + "', '" + phoneNum + "', '" + email + "', '" + address + "', '"
          + firstName + "', '" + lastName + "')";
    else q = "";
    connection.query(q, function (err, results, fields) {
      if (err) { res.end('error adding user: ' + err); return; }
-     res.end("<!DOCTYPE html>\n<html>\n<head>\n<title>Users</title>\n</head>\n<body>\n<p>" + results.affectedRows +
+     res.end("<!DOCTYPE html>\n<html>\n<head>\n<title>Order</title>\n</head>\n<body>\n<p>" + results.affectedRows +
              " record added</p>\n</body>\n</html>\n");
    });
- });
- 
- app.get('/login', function (req, res) {
- 	var email = req.query.email.trim();
- 	var password = req.query.password.trim();
- 	if (email.indexOf(";") == -1) 
- 		var q = "select FirstName from Users where Email='" + email + "' and Password='" + password + "'";
- 	else var q = "";
- 	connection.query(q, function (err, results, fields) {
- 		if (err) {res.end('error logging in: ' + err); return; }
- 		res.end("");
- 	})
- })
- 
- app.get('/viewProducts', function (req, res) {
-	var q = "select * from Products";
-	connection.query(q, function (err, results, fields) {
-	  if (err) { res.end('error retrieving products: ' + err); return;}
-	  var s = "<!DOCTYPE html>\n<html>\n<head>\n<title>Products</title>\n</head>\n<body>\n<table border='1'>\n<tr>";
-     for (var i in fields) s += "<th>" + fields[i].name + "</th>";
-     s += "</tr>\n";
-     for (var row in results) {
-       s += "<tr>\n";
-       for (var col in results[row])
-         s += "<td>" + results[row][col] + "</td>\n";
-       s += "</tr>\n";
-    }
-    s += "</table>\n</body>\n</html>\n";
-    res.send(s);
-	 });
  });
 
 var svr = http.createServer(app);
 svr.on('error', function(err) {console.log('Server: ' + err);});
-svr.listen(8080, function() {console.log("Server Online");});
+svr.listen(8002, function() {console.log("Server Online");});
